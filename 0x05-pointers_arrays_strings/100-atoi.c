998a81e959fbed98a641b5d35dfc1496abd03c69
james-eo <jameseokonkwo@gmail.com>
2022-10-24 18:06:08 -0700

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the sring to be printed
+ * Return: 0 if there are no numbers in the string
+ */
+
+int _atoi(char *s)
+{
+	int a;
+
+	while (s != '\0')
+		s++;
+	if (s == a)
+		atoi(s);
+	else
+		return (0);
+}
