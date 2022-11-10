f91e4f47c6aedf5269157f74ac8bfb8cd90ef968
james-eo <jameseokonkwo@gmail.com>
2022-11-10 22:05:13 +0100

    Task 0, 4 and 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - multiplies tow positive numbers
+ * @argc: argument
+ * @argv: argument
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i, j;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
