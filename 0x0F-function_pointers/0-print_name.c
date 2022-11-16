cb20a3765646afcf493749f021f1374e4a19de47
james-eo <jameseokonkwo@gmail.com>
2022-11-16 12:11:51 +0100

    C function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != 0 && f != 0)
+		f(name);
+}
