f12e0c7ad1476ab1649c983325300359688620de
james-eo <jameseokonkwo@gmail.com>
2023-10-28 09:08:09 +0100

    Prints combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,11 @@ int main(void)
 	int my_num;
 
 	for (my_num = 0; my_num < 10; my_num++)
-		if (num != 10)
-		{
-			putchar("%d", my_num);
-			putchar(',');
-			putchar(" ");
-		}
-	putchar("\n");
+	{
+		putchar(my_num + '0');
+		putchar(',');
+		putchar(' ');
+	}	
+	putchar('\n');
 	return (0);
 }
