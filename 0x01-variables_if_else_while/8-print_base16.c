c95d8448fe6f6fd5b299998965d23936bfc77774
james-eo <jameseokonkwo@gmail.com>
2023-10-28 08:50:46 +0100

    Prints numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@ int main(void)
 	int num;
 	char al;
 
-	for (num = 0; num < 16; num++)
-		putchar((num % 10) + 0);
-	for (al = 'a'; al < 'f'; al++)
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+	for (al = 'a'; al < 'g'; al++)
 		putchar(al);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
