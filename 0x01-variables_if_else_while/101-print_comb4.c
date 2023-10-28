934f19ff9054bc2f59900c9e63f81c3525a60042
james-eo <jameseokonkwo@gmail.com>
2023-10-28 10:51:39 +0100

    Prints different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints different combntions of 2 digits
+ * Return: Always 0 (Succss)
+ */
+
+int main(void)
+{
+	int one, two, three;
+
+	for (one = 0; one < 8; one++)
+	{
+		for (two = one + 1; two < 9; two++)
+		{
+			for (three = two + 1; three < 10; three++)
+			{
+				putchar(one + '0');
+				putchar(two + '0');
+				putchar(three + '0');
+				
+				if (one < 7 || two < 8 || three < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
