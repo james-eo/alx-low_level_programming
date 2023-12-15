f7c9e128adc4c3e1408c06ce7785a00d9bbf7d4b
james-eo <jameseokonkwo@gmail.com>
2023-12-15 14:18:42 +0100

    Script that creates a dynamic library from .c files

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
+
+# Compile all .c files into a shared library
+
+# Get the list of .c files in the current directory
+C_FILES=$(ls *.c)
+
+# Compile the files into a shared library
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -shared -fPIC -o liball.so $C_FILES
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Dynamic library liball.so created successfully."
+else
+    echo "Compilation failed. Check for errors."
+fi
 
